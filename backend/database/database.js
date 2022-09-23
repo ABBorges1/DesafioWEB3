c58fe9ea4def95b449c4efec00ce34e1bfbc499a
ABBorges1 <adrielbarborges@gmail.com>
2022-09-23 20:17:04 -0300

    Comit teste deploy

---
+++ backend/database/database.js
@@ -0,0 +1,36 @@
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, child, get } = require('firebase/database')
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCl3UN1U8D3D4RDScc8UrqtjbT2tztsKcI",
+  authDomain: "dbzombie-ad1bd.firebaseapp.com",
+  databaseURL: "https://dbzombie-ad1bd-default-rtdb.firebaseio.com",
+  projectId: "dbzombie-ad1bd",
+  storageBucket: "dbzombie-ad1bd.appspot.com",
+  messagingSenderId: "727374127765",
+  appId: "1:727374127765:web:2ed586f029e4c762a8f19c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+async function getRanking(){
+  const dbRef = ref(getDatabase());
+  const snapshot = await get(child(dbRef, `ranking`))
+  if (snapshot.exists()) {
+    const jogadores = Object.entries(snapshot.val()).map(([key, value]) => ({
+      nome: key,
+      score: value
+    }))
+    return jogadores;
+  } else {
+    return [];
+  }
+}
+
+module.exports = getRanking;
+
+
+
+
---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { get } = require("firebase/database");
+const app = express();
+const getRanking = require('./database/database')
+
+// Ao fazer este comando, a aplicação aceitará arquivor estáticos (nome_da_pasta)
+app.use(express.static('public'));
+// Dizendo ao express usar o EJS com View engine
+app.set("view engine","ejs")
+// Permite que leia dados em formulário enviado via JSON
+//app.use(bodyParser.json())
+//Configurando a porta que o aplicativo vai rodar
+app.listen(3000, ()=>{
+    console.log("App rodando!")
+});
+
+app.get("/", async (req, res)=>{
+
+    const tables = await getRanking()
+    tables.sort((playerA, playerB)=>{
+        if (playerA.score < playerB.score) {
+            return 1
+        }
+        if (playerA.score === playerB.score) {
+            return 0
+        }
+        if (playerA.score > playerB.score) {
+            return -1
+        }
+    })
+    res.render("table",{tables})
+});
+
